ce17337ac7e0cb3a6a0402b6c6d941c07ccf42f4
Ross Hunter <ross.grant.hunter@gmail.com>
2026-04-27 00:11:29 +1200

    netlify class page loading error - trying to fix

--- components/head-loader.js
+++ components/head-loader.js
@@ -5,6 +5,7 @@
 // remain in each page's <head> and are not managed here.
 (function () {
   document.head.insertAdjacentHTML('beforeend',
+    '<link rel="icon" type="image/png" href="/images/logo-icon.png">' +
     '<link rel="preconnect" href="https://fonts.googleapis.com">' +
     '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' +
     '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,300..900;1,9..144,300..900&family=Inter+Tight:wght@300;400;500;600&family=Poppins:wght@400;500;600;700&display=swap">'
